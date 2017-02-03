6332f09b270814ec8ddf00169eb151a51894c3ef
Samin Zachariah <saminzachariah@seattleacademy.org>
2017-02-03 08:53:45 -0800

    fix drone index.js

--- apps/drone/index.js
+++ apps/drone/index.js
@@ -1,15 +1,16 @@
 var alexa = require('alexa-app');
 var request = require('request');
 port = 1535;
-var docURL = "http://math.seattleacademy.org:"+port+"cmd=";
+var docURL = "http://math.seattleacademy.org:"+port+"?cmd=";
 
 // Allow this module to be reloaded by hotswap when changed
 module.change_code = 1; //When 0, you need to relaunch the server
 
 // Define an alexa-app
 
-function tellDrone(command){
-    request(docURL + docId, function(error, response, body) {
+function tellDrone(req, res, cmd){
+    request(docURL + cmd, function(error, response, body) {
+        console.log("docURL & cmd ", docURL + cmd);
         if (!error && response.statusCode == 200) {
             res.card({
                 type: "Standard",
@@ -28,12 +29,13 @@ var app = new alexa.app('drone');
 app.launch(function(req, res) {
     
     res.say("Give the drone a command like take off, land, or flip");
+    res.shouldEndSession(false);
 });
 app.intent('CommandIntent', {
     "slots": { "COMMAND": "LITERAL" },
     "utterances": ["{land|flip|COMMAND}}"]
 }, function(req, res) {
     res.say('You told the drone to ' + req.slot('COMMAND'));
-    tellDrone(req.slot('COMMAND'));
+    tellDrone(req, res, req.slot('COMMAND'));
 });
 module.exports = app;
